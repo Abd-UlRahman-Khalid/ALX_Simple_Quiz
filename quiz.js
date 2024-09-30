8e2a634e2a11e3c7ecd960cce172a32593dbdd8e
Abd-UlRahman <a.khalid5322@gmail.com>
2024-09-30 19:08:02 +0300

    update

--- quiz.js
+++ quiz.js
@@ -7,12 +7,15 @@
 //   }
 // }
 
-document.getElementById("submit-answer").addEventListener("click", function () {
-  isChoice1 = document.getElementById("choice1").checked;
-  if (isChoice1) {
+function checkAnswer() {
+  let correctAnswer = "4";
+  document.querySelector('[name="quiz"]');
+  let userAnswer = document.querySelector('[name="quiz"]').value;
+  if (userAnswer == correctAnswer) {
     document.getElementById("feedback").innerHTML = "Correct! Well done.";
   } else {
     document.getElementById("feedback").innerHTML =
       "That's incorrect. Try again!";
   }
-});
+}
+document.getElementById("submit-answer").addEventListener("click", checkAnswer);
