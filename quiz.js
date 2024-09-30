5b7daa1761ce3b741be379b8948003c144b8b25f
Abd-UlRahman <a.khalid5322@gmail.com>
2024-09-30 19:26:04 +0300

    adding :checked in the querySelector

--- quiz.js
+++ quiz.js
@@ -10,7 +10,8 @@
 function checkAnswer() {
   let correctAnswer = "4";
   document.querySelector('[name="quiz"]');
-  let userAnswer = document.querySelector('[name="quiz"]').value;
+  let userAnswer = document.querySelector('[name="quiz"]:checked').value;
+  document.querySelector('[name="quiz"]').checked;
   if (userAnswer == correctAnswer) {
     document.getElementById("feedback").innerHTML = "Correct! Well done.";
   } else {
